55ba69c2b1af26a11f52ad8922e39f9e36a32529
PHnaves <pnaves001@gmail.com>
2025-03-03 21:16:49 -0300

    Criação de Componentes Form Para Deixar Mais Dinamico ao Criar e Editar Usuarios e Produtos

---
+++ resources/views/products/components/form.blade.php
@@ -0,0 +1,35 @@
+@csrf
+      
+        <div>
+            <label for="name" class="block text-gray-300 font-medium">Nome do Produto</label>
+            <input type="text" value="{{ $product->name ?? old('name') }}" name="name" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white border border-gray-600" required>
+        </div>
+        
+        <div>
+            <label for="description" class="block text-gray-300 font-medium">Descrição</label>
+            <textarea name="description" value="{{ $product->description ?? old('description') }}" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white border border-gray-600 focus:ring-2 focus:ring-indigo-500" required></textarea>
+        </div>
+        
+        <div>
+            <label for="price" class="block text-gray-300 font-medium">Preço</label>
+            <input type="number" value="{{ $product->price ?? old('price') }}" name="price" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white border border-gray-600 focus:ring-2 focus:ring-indigo-500" required>
+        </div>
+        
+        <div>
+            <label for="category" class="block text-gray-300 font-medium">Categoria</label>
+            <select id="category" name="id_category" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white border border-gray-600 focus:ring-2 focus:ring-indigo-500" required>
+                <option value="" disabled selected>Selecione uma categoria</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        
+        <div>
+            <label for="image" class="block text-gray-300 font-medium">URL da Imagem</label>
+            <input type="text" value="{{ $product->image ?? old('image')}}" name="image" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white border border-gray-600 focus:ring-2 focus:ring-indigo-500" required>
+        </div>
+        
+        <div class="flex justify-end">
+            <button type="submit" class="px-6 py-2 rounded-md bg-indigo-600 text-white hover:bg-indigo-700 transition">Cadastrar</button>
+        </div>
---
+++ resources/views/users/components/form.blade.php
@@ -0,0 +1,24 @@
+@csrf
+
+{{-- nome --}}
+<div>
+    <label for="name" class="block text-gray-300 font-medium">Nome</label>
+    <input type="text" value="{{ $user->name ?? old('name') }}" name="name" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white border border-gray-600" required>
+</div>
+
+{{-- email --}}
+<div>
+    <label for="email" class="block text-gray-300 font-medium">Email</label>
+    <input type="email" value="{{ $user->email ?? old('email') }}" name="email" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white border border-gray-600" required>
+</div>
+
+{{-- senha --}}
+<div>
+    <label for="password" class="block text-gray-300 font-medium">Senha</label>
+    <input type="password" name="password" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white border border-gray-600 focus:ring-2 focus:ring-indigo-500" required>
+</div>
+
+{{-- botão para confirmação --}}
+<div class="flex justify-end">
+    <button type="submit" class="px-6 py-2 rounded-md bg-indigo-600 text-white hover:bg-indigo-700 transition">Cadastrar</button>
+</div>
