03bd987ed56abbcf6f4cf228d933dec214e1ac9d
PHnaves <pnaves001@gmail.com>
2025-04-02 21:37:29 -0300

    Atualização do formulário de usuários para incluir validação de erros e mensagens de feedback, além de ajustes na exibição do botão de submissão, que agora muda conforme a ação (Cadastrar ou Atualizar).

--- resources/views/users/components/form.blade.php
+++ resources/views/users/components/form.blade.php
@@ -3,22 +3,33 @@
 {{-- nome --}}
 <div>
     <label for="name" class="block text-gray-300 font-medium mb-1">Nome</label>
-    <input type="text" value="{{ $user->name ?? old('name') }}" name="name" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500" required>
+    <input type="text" value="{{ $user->name ?? old('name') }}" name="name" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 @error('name') border-red-500 @enderror" required>
+    @error('name')
+        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+    @enderror
 </div>
 
 {{-- email --}}
 <div>
     <label for="email" class="block text-gray-300 font-medium mb-1">Email</label>
-    <input type="email" value="{{ $user->email ?? old('email') }}" name="email" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500" required>
+    <input type="email" value="{{ $user->email ?? old('email') }}" name="email" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 @error('email') border-red-500 @enderror" required>
+    @error('email')
+        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+    @enderror
 </div>
 
 {{-- senha --}}
 <div>
     <label for="password" class="block text-gray-300 font-medium mb-1">Senha</label>
-    <input type="password" name="password" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500" required>
+    <input type="password" name="password" class="w-full px-4 py-2 rounded-md bg-gray-700 text-white focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 @error('password') border-red-500 @enderror" {{ isset($user) ? '' : 'required' }}>
+    @error('password')
+        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+    @enderror
 </div>
 
 {{-- botão para confirmação --}}
 <div class="flex justify-end">
-    <button type="submit" class="px-6 py-2 rounded-md bg-indigo-600 text-white hover:bg-indigo-700 transition cursor-pointer">Cadastrar</button>
+    <button type="submit" class="px-6 py-2 rounded-md bg-indigo-600 text-white hover:bg-indigo-700 transition cursor-pointer">
+        {{ isset($user) ? 'Atualizar' : 'Cadastrar' }}
+    </button>
 </div>
