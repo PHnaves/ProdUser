d32ae6cf37e869c0be6fc48e953b520b7124a550
PHnaves <pnaves001@gmail.com>
2025-04-02 21:36:51 -0300

    Refatoração da view de edição de categorias para incluir o formulário a partir de um componente separado, melhorando a organização do código.

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -11,25 +11,11 @@
 <body>
     @include('layouts.nav')
 
-
     <div class="max-w-4xl mx-auto mt-10 bg-gray-800 p-6 rounded-lg shadow-md">
         <h2 class="text-white text-2xl font-semibold mb-4">Editar Categoria</h2> 
-        <form action="{{ route('categories.update', $category->id)}}" method="post">
-            @csrf
+        <form action="{{ route('categories.update', $category->id) }}" method="post">
             @method('put')
-            <div>
-                <label for="name" class="block text-gray-300 font-medium mb-1">Nome da Categoria</label>
-                <input type="text" value="{{ $category->name }}" name="name" class="w-full px-4 py-2 mb-5 rounded-md bg-gray-700 text-white focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500" required>
-            </div>
-            
-            <div>
-                <label for="name" class="block text-gray-300 font-medium mb-1">Descrição da Categoria</label>
-                <input type="text" value="{{ $category->description }}" name="description" class="w-full px-4 py-2 mb-5 rounded-md bg-gray-700 text-white focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500" required>
-            </div>
-
-            <div class="flex justify-end">
-                <button type="submit" class="px-6 py-2 rounded-md bg-indigo-600 text-white hover:bg-indigo-700 transition cursor-pointer">Confirmar</button>
-            </div>
+            @include('categories.components.form')
         </form>
     </div>
     
